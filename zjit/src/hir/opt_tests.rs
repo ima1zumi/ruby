1b82e3355df14e5a4e1474243d9a7f70b2f71f58
nozomemein <b8yukifsukeo999n@gmail.com>
2026-04-16 07:09:06 +0900

    ZJIT: Stabilize polymorphic getivar tests
    Avoid receiver shapes near the embedded/heap boundary in the
    polymorphic getivar tests.
    
    Make both receivers clearly heap-backed so stats and dev builds still
    exercise distinct @foo slots without depending on embedded_p().

--- zjit/src/hir/opt_tests.rs
+++ zjit/src/hir/opt_tests.rs
@@ -7912,12 +7912,13 @@ mod hir_opt_tests {
         set_call_threshold(3);
         eval(r#"
             class C
-              def foo_then_bar
+              def foo_then_many
                 @foo = 1
+                1000.times { |i| instance_variable_set(:"@v#{i}", i) }
                 @bar = 2
               end
 
-              def bar_then_foo
+              def many_then_foo
                 1000.times { |i| instance_variable_set(:"@v#{i}", i) }
                 @bar = 3
                 @foo = 4
@@ -7927,14 +7928,14 @@ mod hir_opt_tests {
             end
 
             O1 = C.new
-            O1.foo_then_bar
+            O1.foo_then_many
             O2 = C.new
-            O2.bar_then_foo
+            O2.many_then_foo
             O1.foo
             O2.foo
         "#);
         assert_snapshot!(hir_string_proc("C.instance_method(:foo)"), @"
-        fn foo@<compiled>:14:
+        fn foo@<compiled>:15:
         bb1():
           EntryPoint interpreter
           v1:BasicObject = LoadSelf
@@ -7986,12 +7987,13 @@ mod hir_opt_tests {
         set_call_threshold(6);
         eval(r#"
             class C
-              def foo_then_bar
+              def foo_then_many
                 @foo = 1
+                1000.times { |i| instance_variable_set(:"@v#{i}", i) }
                 @bar = 2
               end
 
-              def bar_then_foo
+              def many_then_foo
                 1000.times { |i| instance_variable_set(:"@v#{i}", i) }
                 @bar = 3
                 @foo = 4
@@ -8001,9 +8003,9 @@ mod hir_opt_tests {
             end
 
             O1 = C.new
-            O1.foo_then_bar
+            O1.foo_then_many
             O2 = C.new
-            O2.bar_then_foo
+            O2.many_then_foo
             O1.foo
             O1.foo
             O1.foo
@@ -8011,7 +8013,7 @@ mod hir_opt_tests {
             O2.foo
         "#);
         assert_snapshot!(hir_string_proc("C.instance_method(:foo)"), @"
-        fn foo@<compiled>:14:
+        fn foo@<compiled>:15:
         bb1():
           EntryPoint interpreter
           v1:BasicObject = LoadSelf
